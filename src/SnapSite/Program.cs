10c84574586f1fa98f36b6f2cdc4e0f1536d86ad
Farshad Momtaz <fdmomtaz@gmail.com>
2025-03-02 21:32:05 -0800

    WIP

--- src/SnapSite/Program.cs
+++ src/SnapSite/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace SnapSite;
+﻿using System.Text.Json;
+using SnapSite.Models;
+
+namespace SnapSite;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Options? options = SimpleParseOptions(args);
+        if (options == null)
+        {
+            Console.WriteLine("Invalid options");
+            return;
+        }
+
+        
+
         Console.WriteLine("Hello there!");
+    }
+
+    // temp method to parse options from a file
+    public static Options? SimpleParseOptions(string[] args)
+    {
+        if (args.Length != 1)
+            return null;
+
+        if (!File.Exists(args[0]))
+            return null;
+
+        return JsonSerializer.Deserialize<Options>(File.ReadAllText(args[0]));
     }
 }
